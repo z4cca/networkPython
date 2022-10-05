028580e182f054bfaec409c3ccf1229eaecb1fe8
zikZira <jojoe@localhost.localdomain>
2022-10-05 14:43:57 -0300

    Create sniffer

---
+++ blackhatPython/pck_sniffer/sniffer.py
@@ -0,0 +1,24 @@
+import socket
+import os
+
+# host to listen on
+HOST = '127.0.0.1'
+
+def main():
+    # create raw socket, bin to public interface
+    if os.name == 'nt':
+        socket_protocol = socket.IPPROTO_IP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
+
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+    sniffer.bind((HOST, o))
+    # include the IP header in the capture
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+
+    if __name__ == '__main__':
+        main()
+
