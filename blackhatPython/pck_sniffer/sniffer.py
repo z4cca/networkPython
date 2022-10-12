3030dd1b3b8924e94136f0e4830e5a41050f4ca2
zikZira <jojoe@localhost.localdomain>
2022-10-12 12:37:18 -0300

    Updated sniffer, Add IP Decoder

--- blackhatPython/pck_sniffer/sniffer.py
+++ blackhatPython/pck_sniffer/sniffer.py
@@ -2,7 +2,7 @@ import socket
 import os
 
 # host to listen on
-HOST = '127.0.0.1'
+HOST = '192.168.0.17'
 
 def main():
     # create raw socket, bin to public interface
@@ -12,13 +12,19 @@ def main():
         socket_protocol = socket.IPPROTO_ICMP
 
     sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
-    sniffer.bind((HOST, o))
+    sniffer.bind((HOST, 0))
     # include the IP header in the capture
     sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
     if os.name == 'nt':
         sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
 
-    if __name__ == '__main__':
-        main()
+    # Read one packet
+    print(sniffer.recvfrom(65565))
 
+    # If OS == Windows: Turn off promiscuous mode
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+
+if __name__ == '__main__':
+        main()
