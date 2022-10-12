c477e0e471de844f3d1a06111531dca50e657513
zikZira <jojoe@localhost.localdomain>
2022-10-12 16:07:46 -0300

    Updated ip_decoder

--- blackhatPython/pck_sniffer/ip_decoder.py
+++ blackhatPython/pck_sniffer/ip_decoder.py
@@ -4,6 +4,7 @@ import socket
 import struct
 import sys
 
+# IP class definition that maps the first 20 bytes of received buffer
 class IP:
     def __init__(self, buff=None):
         header = struct.unpack('<BBHHHBBH4s4s', buff)
@@ -20,7 +21,7 @@ class IP:
         self.src = header[8]
         self.dst = header[9]
 
-        # Readable IP addr
+        # Readable IP addr: Continually reading packets and parse their info
         self.src_address = ipaddress.ip_address(self.src)
         self.dst_address = ipaddress.ip_address(self.dst)
 
@@ -32,39 +33,59 @@ class IP:
             print('%s No protocol for %s' % (e, self.protocol_num))
         self.protocol = str(self.protocol_num)
 
-    def sniff(host):
-        if os.name == 'nt':
-            socket_protocol = socket.IPPROTO_IP
-        else:
-            socket_protocol = socket.IPPROTO_ICMP
+class ICMP:
+    def __init__(self, buff):
+        header = struct.unpack('<BBHHH', buff)
+        self.type = header[0]
+        self.code = header[1]
+        self.sum = header[2]
+        self.id = header[3]
+        self.seq = header[4]
 
-        sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
-        sniffer.bind((HOST, 0))
-        sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+def sniff(host):
+    if os.name == 'nt':
+        socket_protocol = socket.IPPROTO_IP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
 
-        if os.name == 'nt':
-            sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+    sniffer.bind((host, 0))
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
-        try:
-            while True:
-                # Read one packet
-                raw_buffer = sniffer.recvfrom(65535)[0]
-                # Create IP header from first 20 bytes
-                ip_header = IP(raw_buffer[0:20])
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    
+    try:
+        while True:
+            # Read one packet
+            raw_buffer = sniffer.recvfrom(65535)[0]
+            # Create IP header from first 20 bytes
+            ip_header = IP(raw_buffer[0:20])
+            #If header is ICMP
+            if ip_header.protocol == 'ICMP':
                 # Print detected protocol and hosts
                 print('Protocol: %s %s -> %s' % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
+                print(f'Version: {ip_header.ver}') 
+                print('Header Lenght: {ip_header.len} Time to Live: {ip_header.ttl}')
+                
+                # Calculate where ICMP packet starts
+                offset = ip_header.ihl * 4
+                buf = raw_buffer[offset:offset + 8]
+                # Create ICMP structure
+                icmp_header = ICMP(buf)
+                print('ICMP -> Type: %s Code: %s\n' % (icmp_header.type, icmp_header.code))
+    
+    
+    except KeyboardInterrupt:
+        # if OS == Windows: Turn off promiscuous mode
+        if os.name == 'nt':
+            sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+        sys.exit()
 
-
-        except KeyboardInterrupt:
-            # if OS == Windows: Turn off promiscuous mode
-            if os.name == 'nt':
-                sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
-            sys.exit()
-
-if __name == '__main__':
+# Defines main function
+if __name__ == '__main__':
     if len(sys.argv) == 2:
         host = sys.argv[1]
     else:
-        host = '127.0.0.1'
-
+        host = '192.168.0.17'
     sniff(host)
